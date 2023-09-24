068ad18593f3b229edc814c15b322996c2911d63
LeverActionGT <63264121+LeverActionGT@users.noreply.github.com>
2023-09-24 14:18:12 -0400

    Working OLED
    OLED is working and scanner script added

--- arduino/RobotArmFirmware/RobotArmFirmware.ino
+++ arduino/RobotArmFirmware/RobotArmFirmware.ino
@@ -8,11 +8,10 @@
 // OLED display
 #define SCREEN_WIDTH 128 // OLED display width
 #define SCREEN_HEIGHT 64 // OLED display height
+#define SCREEN_ADDRESS 0x3C
 #define OLED_RESET -1
-#define SCREEN_ADDRESS 0x3D
-#define OLED_SDA 25
-#define OLED_SCL 24
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, Wire2, OLED_RESET);
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire2, OLED_RESET);
 
 // TODO: create a SERIAL pointer that points to the serial port
 
@@ -25,14 +24,44 @@ Encoder knob(ROTARY_ENCODER_PIN_A, ROTARY_ENCODER_PIN_B);
 
 void setup()
 {
+    // Serial port
+    Serial.begin(9600);
+    Serial.println("Serial port initialized");
 
     // Rotary encoder
     pinMode(ROTARY_ENCODER_PIN_A, INPUT);
     pinMode(ROTARY_ENCODER_PIN_B, INPUT);
     pinMode(ROTARY_ENCODER_PIN_SW, INPUT);
+    Serial.println("Rotary encoder initialized");
+
+    // OLED display
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+    {
+        Serial.println("SSD1306 allocation failed");
+        for (;;)
+            ;
+    }
+    Serial.println("OLED display initialized");
+
+    display.clearDisplay();
+
+    Serial.println("Setup complete");
 }
 
 void loop()
 {
-    // int pos = knob.read()/-2;
+    int pos = knob.read()/-2;
+    if (pos != knobPos)
+    {
+        knobPos = pos;
+        Serial.println(knobPos);
+    }
+
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 28);
+    display.println("Knob: " + String(knobPos));
+    display.display();
+    delay(100);
+    display.clearDisplay();
 }
